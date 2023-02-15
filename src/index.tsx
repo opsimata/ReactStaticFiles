b83e3dd8c5eecc00062b4451529736f197a0d4fb
Pedro <pedro.enrique1599@icloud.com>
2023-02-15 15:39:56 -0300

    nav logo and styles update

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Menu from './pages/Menu';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <div>Aluroni</div>
+    <Menu />
   </React.StrictMode>
 );
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -3,8 +3,9 @@ import styles from "./Menu.module.scss";
 export default function Menu() {
     return(
         <main>
-            <nav className="menu">
-
+            <nav className={styles.menu}>
+                <img src="../../assets/logo.svg" alt="Aluroni logo" />
+                Aluroni
             </nav>
         </main>
     )
