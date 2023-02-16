1b2adc754e0bd7db1c23742108e0573ff53ab0ab
Pedro <pedro.enrique1599@icloud.com>
2023-02-16 03:24:18 -0300

    menu style, title and finder module added

---
+++ src/pages/Menu/Finder/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./Finder.module.scss";
+import { CgSearch } from "react-icons/cg";
+
+interface Props {
+  find: string;
+  setFinder: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Finder({ find, setFinder }: Props) {
+  return (
+    <div className={styles.finder}>
+      <input
+        value={find}
+        onChange={(event) => setFinder(event.target.value)}
+        type="text"
+      />
+      <CgSearch size={20} color="#4C4D5E" />
+    </div>
+  );
+}
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,17 +1,22 @@
 import styles from "./Menu.module.scss";
 import { ReactComponent as Logo } from "assets/img/logo.svg";
+import Finder from "./Finder";
+import { useState } from "react";
 
 export default function Menu() {
+  const [find, setFinder] = useState("");
   return (
     <main>
-      <nav className={styles.menu}>
+      <nav className={styles.menunav}>
         <Logo />
       </nav>
       <header className={styles.header}>
-        <div className={styles.header__text}>
-          A casa do código e da massa
-        </div>
+        <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
+      <section className={styles.menu}>
+        <h3 className={styles.menu__title}>Cardápio</h3>
+        <Finder find={find} setFinder={setFinder}/>
+      </section>
     </main>
   );
 }
