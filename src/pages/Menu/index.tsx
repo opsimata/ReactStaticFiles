075e794a1b02cdfcdcb8f6f52a6eb0311178e66f
Pedro <pedro.enrique1599@icloud.com>
2023-02-16 02:36:10 -0300

    adjusting styles and adding header

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./Menu.module.scss";
-import { ReactComponent as Logo } from "assets/img/logo.svg"
+import { ReactComponent as Logo } from "assets/img/logo.svg";
 
 export default function Menu() {
   return (
@@ -7,6 +7,11 @@ export default function Menu() {
       <nav className={styles.menu}>
         <Logo />
       </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do código e da massa
+        </div>
+      </header>
     </main>
   );
 }
