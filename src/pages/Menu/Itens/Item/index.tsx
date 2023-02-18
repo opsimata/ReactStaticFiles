dffd11e32e398aaa2064266f75c5ac7c2ac0c39c
Pedro <pedro.enrique1599@icloud.com>
2023-02-18 04:26:45 -0300

    more styles to item component added

--- src/pages/Menu/Itens/Item/index.tsx
+++ src/pages/Menu/Itens/Item/index.tsx
@@ -1,15 +1,16 @@
 import logo from "assets/img/logo.svg";
 import styles from "./Item.module.scss";
 import menu from "../itens.json";
+import classNames from "classnames";
 
 type Props = typeof menu[0];
 
 export default function Items(props: Props) {
-  const { title, description, category, size, serving, price } = props;
+  const { title, description, category, size, serving, price, photo } = props;
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem}>
-        <img src={logo} alt={title} />
+        <img src={photo} alt={title} />
       </div>
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>
@@ -17,7 +18,14 @@ export default function Items(props: Props) {
           <p>{description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__tipo}>{category.label}</div>
+          <div 
+          className={classNames({
+            [styles.item__tipo]: true,
+            [styles[`item__tipo__${category.label.toLowerCase()}`]]:true
+          })}
+          >
+          {category.label}
+          </div>
           <div className={styles.item__porcao}>{size} g</div>
           <div className={styles.item__qtdpessoas}>Serve {serving} {serving === 1 ? "pessoa" : "pessoas"}</div>
           <div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
