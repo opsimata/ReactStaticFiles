c6783c1e2133883d7c6c70adf44d65d993e045ed
Pedro <pedro.enrique1599@icloud.com>
2023-02-18 02:22:04 -0300

    items module template and style added

--- src/pages/Menu/Itens/Item/index.tsx
+++ src/pages/Menu/Itens/Item/index.tsx
@@ -1,7 +1,24 @@
+import logo from "assets/img/logo.svg";
+import styles from "./Item.module.scss";
+
 export default function Items() {
-    return(
-        <div>
-            Item
+  return (
+    <div className={styles.item}>
+      <div className={styles.item__imagem}>
+        <img src={logo} alt="imagem" />
+      </div>
+      <div className={styles.item__descricao}>
+        <div className={styles.item__titulo}>
+          <h2>Macarrão</h2>
+          <p>desc</p>
         </div>
-    )
+        <div className={styles.item__tags}>
+          <div className={styles.item__tipo}>masa</div>
+          <div className={styles.item__porcao}>400</div>
+          <div className={styles.item__qtdpessoas}>serve 343424</div>
+          <div className={styles.item__valor}>R$ 78</div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Menu/Itens/index.tsx
+++ src/pages/Menu/Itens/index.tsx
@@ -1,8 +1,13 @@
 import menu from "./itens.json";
-import Item from "./Item"
+import Item from "./Item";
+import styles from "./Itens.module.scss";
 
 export default function Itens() {
-  return <div>{menu.map(item => (
-    <Item key={item.id}/>
-  ))}</div>;
+  return (
+    <div className={styles.itens}>
+      {menu.map((item) => (
+        <Item key={item.id} />
+      ))}
+    </div>
+  );
 }
