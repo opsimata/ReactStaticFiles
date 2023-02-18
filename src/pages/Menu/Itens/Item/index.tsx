40a53a6e19ed54e34708311522b01695a5f625cb
Pedro <pedro.enrique1599@icloud.com>
2023-02-17 23:47:50 -0300

    items components start

---
+++ src/pages/Menu/Itens/Item/index.tsx
@@ -0,0 +1,7 @@
+export default function Items() {
+    return(
+        <div>
+            Item
+        </div>
+    )
+}
--- src/pages/Menu/Itens/index.tsx
+++ src/pages/Menu/Itens/index.tsx
@@ -1,4 +1,5 @@
 import menu from "./itens.json";
+import Item from "./Item"
 
 export default function Itens() {
   return <div>{menu.map(item => (
